e848600bc08cd413a69a24b6582aa460a1630435
mdif <710740608@qq.com>
2018-12-31 21:12:45 +0800

    修复切换页面的BUG

--- main.js
+++ main.js
@@ -2,14 +2,29 @@ let n
 let imgAmount = $('#imgVessel>img').length
 init()
 
-setInterval((e) => {
+let timer = setInterval((e) => {
     makeLeave(getImg(n))
         .one('transitionend', (e) => {
             makeEnter($(e.currentTarget))
         })
     makeCurrent(getImg(n+1))
     n += 1
-}, 2000)
+}, 3000)
+
+document.addEventListener('visibilitychange',()=>{
+    if(document.hidden){
+        window.clearInterval(timer)
+    }else{
+        timer = setInterval((e) => {
+            makeLeave(getImg(n))
+                .one('transitionend', (e) => {
+                    makeEnter($(e.currentTarget))
+                })
+            makeCurrent(getImg(n+1))
+            n += 1
+        }, 3000)
+    }
+})
 
 function getImg(n){
     return $(`#imgVessel>img:nth-child(${imgIndex(n)}`)
